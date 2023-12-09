14d56facb9cb9b03470a43dda80c7c41ac09344a
Sebastien Nicolet <snicolet95@gmail.com>
2023-12-09 17:25:56 +0100

    black

--- tortoise_pagination/pagination.py
+++ tortoise_pagination/pagination.py
@@ -12,8 +12,8 @@ M = TypeVar("M")
 
 # https://github.com/pydantic/pydantic/pull/595
 class Page(BaseModel, Generic[T], ABC):
-    """Represent a Page of T
-    """
+    """Represent a Page of T"""
+
     count: NonNegativeInt = 0
     items: list[T]
 
@@ -22,24 +22,19 @@ class Page(BaseModel, Generic[T], ABC):
 
 
 class Pagination(BaseModel):
-    """Represents a Pagination request
-    """
+    """Represents a Pagination request"""
+
     offset: NonNegativeInt | None = None
     limit: NonNegativeInt = 10
 
     @classmethod
     def from_query(
-        cls,
-        offset: NonNegativeInt | None = Query(None),
-        limit: NonNegativeInt | None = Query(10)
+        cls, offset: NonNegativeInt | None = Query(None), limit: NonNegativeInt | None = Query(10)
     ) -> "Pagination":
         try:
             return cls(offset=offset, limit=limit)
         except ValidationError as error:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=error.errors()
-            )
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error.errors())
 
     def paginate_queryset(self, queryset: QuerySet[M]) -> QuerySet[M]:
         if self.limit is not None:
@@ -48,18 +43,13 @@ class Pagination(BaseModel):
             queryset = queryset.offset(self.offset)
         return queryset
 
-    async def paginated_response(
-        self,
-        queryset: QuerySet,
-        schema: Type[PydanticModel]
-    ) -> Page:
-        """Returns a page from the given queryset
-        """
+    async def paginated_response(self, queryset: QuerySet, schema: Type[PydanticModel]) -> Page:
+        """Returns a page from the given queryset"""
         paginated_queryset = self.paginate_queryset(queryset)
         pagination_class = Page[schema]
         limit = paginated_queryset._limit
         page = pagination_class(
             items=await schema.from_queryset(paginated_queryset) if limit else [],
-            count=await queryset.count()
+            count=await queryset.count(),
         )
         return page
