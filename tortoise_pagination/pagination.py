c334da4337e3a63f466c99fdad2dc826eb9e1d42
Sebastien Nicolet <snicolet95@gmail.com>
2024-07-31 15:23:18 +0200

    updated dependencies + count and fetch are now done in asyncio concurency

--- tortoise_pagination/pagination.py
+++ tortoise_pagination/pagination.py
@@ -1,8 +1,9 @@
 from abc import ABC
+from asyncio import TaskGroup
 from typing import Generic, Type, TypeVar
 
 from fastapi import HTTPException, Query, status
-from pydantic import BaseModel, NonNegativeInt, ValidationError
+from pydantic import BaseModel, Field, NonNegativeInt, ValidationError
 from tortoise.contrib.pydantic import PydanticModel
 from tortoise.queryset import QuerySet
 
@@ -15,10 +16,14 @@ class Page(BaseModel, Generic[T], ABC):
     """Represent a Page of T"""
 
     count: NonNegativeInt = 0
-    items: list[T]
+    items: list[T] = Field(default_factory=lambda: [])
 
     class Config:
         arbitrary_types_allowed = True
+
+
+async def _count_queryset(queryset: QuerySet) -> int:
+    return await queryset.count()
 
 
 class Pagination(BaseModel):
@@ -29,12 +34,16 @@ class Pagination(BaseModel):
 
     @classmethod
     def from_query(
-        cls, offset: NonNegativeInt | None = Query(None), limit: NonNegativeInt | None = Query(10)
+        cls,
+        offset: NonNegativeInt | None = Query(None),
+        limit: NonNegativeInt | None = Query(10),
     ) -> "Pagination":
         try:
             return cls(offset=offset, limit=limit)
         except ValidationError as error:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error.errors())
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=error.errors()
+            )
 
     def paginate_queryset(self, queryset: QuerySet[M]) -> QuerySet[M]:
         if self.limit is not None:
@@ -43,13 +52,21 @@ class Pagination(BaseModel):
             queryset = queryset.offset(self.offset)
         return queryset
 
-    async def paginated_response(self, queryset: QuerySet, schema: Type[PydanticModel]) -> Page:
+    async def paginated_response(
+        self, queryset: QuerySet, schema: Type[PydanticModel]
+    ) -> Page:
         """Returns a page from the given queryset"""
         paginated_queryset = self.paginate_queryset(queryset)
         pagination_class = Page[schema]
         limit = paginated_queryset._limit
+
+        async with TaskGroup() as tg:
+            if limit > 0:
+                items = tg.create_task(schema.from_queryset(paginated_queryset))
+            count = tg.create_task(_count_queryset(queryset))
+
         page = pagination_class(
-            items=await schema.from_queryset(paginated_queryset) if limit else [],
-            count=await queryset.count(),
+            items=(items.result() or []) if limit > 0 else [],
+            count=count.result(),
         )
         return page
